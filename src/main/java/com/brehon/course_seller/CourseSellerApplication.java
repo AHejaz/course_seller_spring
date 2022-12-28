d36c797c4ecde0902e9604b258f8d54ef3197810
= <=>
2022-12-28 10:10:38 +0330

    first commit

---
+++ src/main/java/com/brehon/course_seller/CourseSellerApplication.java
@@ -0,0 +1,13 @@
+package com.brehon.course_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSellerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brehon/course_seller/CourseSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brehon.course_seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
